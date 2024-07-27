23159af6d33003825b68774dc24b02c22c7708db
Yao <978887223@qq.com>
2024-07-27 20:38:16 +0800

    add the logIn button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hi,欢迎来到敏捷看板</p>
+        <button class="start-button" onclick="startAndLogIn()">Start and Log In</button>
         <body>
           <div class="scale-up-ver-bottom">Hello, world!</div>
         </body>
